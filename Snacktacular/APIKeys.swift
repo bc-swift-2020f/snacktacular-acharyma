1bb0d1fa04e916faa99db69deb9dd63a2e0fa0c6
Manogya Acharya <acharyma@bc.edu>
2020-11-02 15:03:16 -0500

    gitignore should delete this

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Manogya Acharya on 11/2/20.
+//
+
+import Foundation
+
+struct  APIKeys {
+    static let googlePlacesKey = "AIzaSyArGBSpuyo6iAugrH2re9HKgC40eInnWfI"
+}
